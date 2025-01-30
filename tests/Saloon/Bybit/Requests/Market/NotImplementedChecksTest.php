43403da4b67363479093720e9b055365de8281b1
Allan Mariucci Carvalho <allan.m.carvalho@outlook.com>
2025-01-30 13:34:28 -0300

    improved not implemented check for market

--- tests/Saloon/Bybit/Requests/Market/NotImplementedChecksTest.php
+++ tests/Saloon/Bybit/Requests/Market/NotImplementedChecksTest.php
@@ -5,32 +5,24 @@ use BybitApi\Facades\Market;
 
 it('check if all not implemented tests throw exception', function () {
     $market = Market::actingAs($this->defaultActor());
+    $commonError = "Endpoint not implemented yet on 'BybitApi\Groups\Market::";
 
     expect(fn () => $market->getOrderBook())
-        ->toThrow(NotImplementedYetException::class,
-            "Endpoint not implemented yet on 'BybitApi\Groups\Market::getOrderBook'.")
+        ->toThrow(NotImplementedYetException::class, "{$commonError}getOrderBook'.")
         ->and(fn () => $market->getFundingRateHistory())
-        ->toThrow(NotImplementedYetException::class,
-            "Endpoint not implemented yet on 'BybitApi\Groups\Market::getFundingRateHistory'.")
+        ->toThrow(NotImplementedYetException::class, "{$commonError}getFundingRateHistory'.")
         ->and(fn () => $market->getPublicRecentTradingHistory())
-        ->toThrow(NotImplementedYetException::class,
-            "Endpoint not implemented yet on 'BybitApi\Groups\Market::getPublicRecentTradingHistory'.")
+        ->toThrow(NotImplementedYetException::class, "{$commonError}getPublicRecentTradingHistory'.")
         ->and(fn () => $market->getOpenInterest())
-        ->toThrow(NotImplementedYetException::class,
-            "Endpoint not implemented yet on 'BybitApi\Groups\Market::getOpenInterest'.")
+        ->toThrow(NotImplementedYetException::class, "{$commonError}getOpenInterest'.")
         ->and(fn () => $market->getHistoricalVolatility())
-        ->toThrow(NotImplementedYetException::class,
-            "Endpoint not implemented yet on 'BybitApi\Groups\Market::getHistoricalVolatility'.")
+        ->toThrow(NotImplementedYetException::class, "{$commonError}getHistoricalVolatility'.")
         ->and(fn () => $market->getInsurance())
-        ->toThrow(NotImplementedYetException::class,
-            "Endpoint not implemented yet on 'BybitApi\Groups\Market::getInsurance'.")
+        ->toThrow(NotImplementedYetException::class, "{$commonError}getInsurance'.")
         ->and(fn () => $market->getRiskLimit())
-        ->toThrow(NotImplementedYetException::class,
-            "Endpoint not implemented yet on 'BybitApi\Groups\Market::getRiskLimit'.")
+        ->toThrow(NotImplementedYetException::class, "{$commonError}getRiskLimit'.")
         ->and(fn () => $market->getDeliveryPrice())
-        ->toThrow(NotImplementedYetException::class,
-            "Endpoint not implemented yet on 'BybitApi\Groups\Market::getDeliveryPrice'.")
+        ->toThrow(NotImplementedYetException::class, "{$commonError}getDeliveryPrice'.")
         ->and(fn () => $market->getLongShortRatio())
-        ->toThrow(NotImplementedYetException::class,
-            "Endpoint not implemented yet on 'BybitApi\Groups\Market::getLongShortRatio'.");
+        ->toThrow(NotImplementedYetException::class, "{$commonError}getLongShortRatio'.");
 });
