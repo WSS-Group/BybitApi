214336dd2df72486c2dda74baf93d010a443c27d
allanmcarvalho <12012197+allanmcarvalho@users.noreply.github.com>
2025-02-05 19:49:34 +0000

    Pint style fix

--- tests/Fixtures/Bybit/Position/SetLeverage/NotModifiedFixture.php
+++ tests/Fixtures/Bybit/Position/SetLeverage/NotModifiedFixture.php
@@ -12,8 +12,8 @@ class NotModifiedFixture extends Fixture
         $current = now();
 
         return [
-            "retCode" => 110043,
-            "retMsg" => "leverage not modified",
+            'retCode' => 110043,
+            'retMsg' => 'leverage not modified',
             'result' => [],
             'retExtInfo' => [],
             'time' => $current->getTimestampMs(),
@@ -28,23 +28,23 @@ class NotModifiedFixture extends Fixture
     public function headers(PendingRequest $pendingRequest): array
     {
         return [
-            "Content-Type" => "application/json; charset=utf-8",
-            "Content-Length" => "100",
-            "Connection" => "keep-alive",
-            "Date" => "Wed, 05 Feb 2025 19:44:01 GMT",
-            "x-cld-src" => "Loc-A",
-            "X-Bapi-Limit" => "10",
-            "X-Bapi-Limit-Status" => "9",
-            "X-Bapi-Limit-Reset-Timestamp" => "1738784641555",
-            "Ret_code" => "110043",
-            "Cache-Control" => "no-store",
-            "Traceid" => "63db2a04618854d262db6562b19ac7b6",
-            "Timenow" => "1738784641555",
-            "Server" => "Openresty",
-            "X-Cache" => "Miss from cloudfront",
-            "Via" => "1.1 65f7295ff05cf36f1a9f5c741069f294.cloudfront.net (CloudFront)",
-            "X-Amz-Cf-Pop" => "GRU3-C2",
-            "X-Amz-Cf-Id" => "o6cvw3L9hhcWk9Jyo2lJ54TpVsKpBmxhc3CUb_Tn2-i88a2lmWFKyg==",
+            'Content-Type' => 'application/json; charset=utf-8',
+            'Content-Length' => '100',
+            'Connection' => 'keep-alive',
+            'Date' => 'Wed, 05 Feb 2025 19:44:01 GMT',
+            'x-cld-src' => 'Loc-A',
+            'X-Bapi-Limit' => '10',
+            'X-Bapi-Limit-Status' => '9',
+            'X-Bapi-Limit-Reset-Timestamp' => '1738784641555',
+            'Ret_code' => '110043',
+            'Cache-Control' => 'no-store',
+            'Traceid' => '63db2a04618854d262db6562b19ac7b6',
+            'Timenow' => '1738784641555',
+            'Server' => 'Openresty',
+            'X-Cache' => 'Miss from cloudfront',
+            'Via' => '1.1 65f7295ff05cf36f1a9f5c741069f294.cloudfront.net (CloudFront)',
+            'X-Amz-Cf-Pop' => 'GRU3-C2',
+            'X-Amz-Cf-Id' => 'o6cvw3L9hhcWk9Jyo2lJ54TpVsKpBmxhc3CUb_Tn2-i88a2lmWFKyg==',
         ];
     }
 }
--- tests/Fixtures/Bybit/Position/SetLeverage/OkFixture.php
+++ tests/Fixtures/Bybit/Position/SetLeverage/OkFixture.php
@@ -28,22 +28,22 @@ class OkFixture extends Fixture
     public function headers(PendingRequest $pendingRequest): array
     {
         return [
-            "Content-Type" => "application/json; charset=utf-8",
-            "Content-Length" => "76",
-            "Connection" => "keep-alive",
-            "Date" => "Wed, 05 Feb 2025 19:43:05 GMT",
-            "x-cld-src" => "Loc-A",
-            "X-Bapi-Limit" => "10",
-            "X-Bapi-Limit-Status" => "9",
-            "X-Bapi-Limit-Reset-Timestamp" => "1738784585265",
-            "Ret_code" => "0",
-            "Traceid" => "515ff6a30d3914fd724fb251228bf14e",
-            "Timenow" => "1738784585266",
-            "Server" => "Openresty",
-            "X-Cache" => "Miss from cloudfront",
-            "Via" => "1.1 1e1ba059ef27f31424c8abf622adfb8e.cloudfront.net (CloudFront)",
-            "X-Amz-Cf-Pop" => "GRU3-C2",
-            "X-Amz-Cf-Id" => "WbCD0S82z7i3fbk9xq3adip3D4m6zubaGTmzbYEAmeF5FeAG_N0fcA==",
+            'Content-Type' => 'application/json; charset=utf-8',
+            'Content-Length' => '76',
+            'Connection' => 'keep-alive',
+            'Date' => 'Wed, 05 Feb 2025 19:43:05 GMT',
+            'x-cld-src' => 'Loc-A',
+            'X-Bapi-Limit' => '10',
+            'X-Bapi-Limit-Status' => '9',
+            'X-Bapi-Limit-Reset-Timestamp' => '1738784585265',
+            'Ret_code' => '0',
+            'Traceid' => '515ff6a30d3914fd724fb251228bf14e',
+            'Timenow' => '1738784585266',
+            'Server' => 'Openresty',
+            'X-Cache' => 'Miss from cloudfront',
+            'Via' => '1.1 1e1ba059ef27f31424c8abf622adfb8e.cloudfront.net (CloudFront)',
+            'X-Amz-Cf-Pop' => 'GRU3-C2',
+            'X-Amz-Cf-Id' => 'WbCD0S82z7i3fbk9xq3adip3D4m6zubaGTmzbYEAmeF5FeAG_N0fcA==',
         ];
     }
 }
